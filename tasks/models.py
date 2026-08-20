f186edef72887404b3670e37dc85f84fd7046e65
Nirvan <nirvan96awm@gmail.com>
2026-08-20 13:22:32 +0330

    Add task list page

---
+++ tasks/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+
+
+
+# Create your models here.
