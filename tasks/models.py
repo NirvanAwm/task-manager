ef4587ef9e2d3c5eebe89c8c73c6e40e20e99b1f
NirvanAwm <nirvan96awm@gmail.com>
2026-08-21 22:02:46 +0330

    Expand task model

--- tasks/models.py
+++ tasks/models.py
@@ -4,6 +4,32 @@ class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
 
+    class Status(models.TextChoices):
+        TODO = 'TODO', 'Todo'
+        DOING = 'DOING', 'Doing'
+        DONE = 'DONE', 'Done'
+
+    status = models.CharField(
+        max_length=10,
+        choices=Status.choices,
+        default=Status.TODO
+    )
+
+    class Priority(models.TextChoices):
+        LOW = 'LOW', 'Low'
+        MEDIUM = 'MEDIUM', 'Medium'
+        HIGH = 'HIGH', 'High'
+    
+    priority = models.CharField(
+        max_length=10,
+        choices=Priority.choices, 
+        default=Priority.MEDIUM
+    )
+
+    due_date = models.DateField(blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 # Create your models here.
