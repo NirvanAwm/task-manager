e655f0b4545955d4f8005584410b5946545ddd47
Nirvan <nirvan96awm@gmail.com>
2026-08-22 18:38:25 +0330

    Complete task CRUD

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.task_list, name="task_list"),
-    path("create/", views.task_create, name="task_create")
+    path("create/", views.task_create, name="task_create"),
+    path("<int:task_id>/edit/", views.task_edit, name="task_edit"),
+    path("<int:task_id>/delete", views.task_delete, name="task_delete")
 ]
