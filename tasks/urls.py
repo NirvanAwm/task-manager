22cc47df429edfcd8572d6602cad327cdf3e0138
NirvanAwm <nirvan96awm@gmail.com>
2026-08-28 15:16:54 +0330

    Improving UI design and show username

--- tasks/urls.py
+++ tasks/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("<int:task_id>/edit/", views.task_edit, name="task_edit"),
     path("<int:task_id>/delete", views.task_delete, name="task_delete"),
     path("<int:task_id>/", views.task_detail, name="task_detail"),
+    path("logout/", views.logout_view, name="logout")
 ]
